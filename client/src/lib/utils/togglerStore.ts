c8cdab5b4613dcfdaa5d0f60a22263361f1be364
Kerby Keith Aquino <hello@kurojifusky.com>
2024-08-02 12:53:40 +0800

    svelte store stuff

--- client/src/lib/utils/togglerStore.ts
+++ client/src/lib/utils/togglerStore.ts
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store"
 
-const togglerStore = (state: boolean) => {
+export const togglerStore = (state: boolean) => {
   const { subscribe, update } = writable(state)
 
   const toggleState = () => update((prevState) => (prevState = !prevState))
